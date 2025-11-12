3582e6e51481ad29cb0bd7fd69adba3ff5edf2c9
unknown <Cesar.perez.garcia9@gmail.com>
2025-11-11 18:49:20 -0600

    Se agregaron archivos para practicar, un .c y un .cpp

---
+++ Codigo en C++.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+void printWelcome() {
+    std::cout << "Bienvenido al sistema de control LED (C++)" << std::endl;
+}
+
+int main() {
+    printWelcome();
+    return 0;
+}
---
+++ Driver .c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void start_driver() {
+    printf("Inicializando controlador de hardware (C)\n");
+}
+
+int main() {
+    start_driver();
+    return 0;
+}
